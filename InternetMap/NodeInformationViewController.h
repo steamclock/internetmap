fd13d84c17c7f936d4a4ef288e47d5e86d28936d
Angelina Fabbro <angelina@steamclock.com>
2012-12-04 13:44:41 -0800

    Reorganize UI group in XCode a little bit, create viewcontroller for nodeinfo

---
+++ InternetMap/NodeInformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NodeInformationViewController.h
+//  InternetMap
+//
+//  Created by Angelina Fabbro on 12-12-04.
+//  Copyright (c) 2012 Peer1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface NodeInformationViewController : ViewController
+
+@end
---
+++ InternetMap/NodeInformationViewController.m
@@ -0,0 +1,38 @@
+//
+//  NodeInformationViewController.m
+//  InternetMap
+//
+//  Created by Angelina Fabbro on 12-12-04.
+//  Copyright (c) 2012 Peer1. All rights reserved.
+//
+
+#import "NodeInformationViewController.h"
+
+@interface NodeInformationViewController ()
+
+@end
+
+@implementation NodeInformationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
