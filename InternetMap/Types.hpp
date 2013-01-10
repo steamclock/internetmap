e87e3b95731ef852bbfe23260d67aaa2dcb41e1c
avf <alexander@steamclocksoftware.com>
2013-01-10 10:14:36 -0800

    fixed bug in color constructor

--- InternetMap/Types.hpp
+++ InternetMap/Types.hpp
@@ -50,7 +50,7 @@ struct Colour {
     }
     
     Colour(float R, float G, float B, float A) {
-        r = R= g = G; b = B; a = A;
+        r = R; g = G; b = B; a = A;
     }
     
     float r, g, b, a;
