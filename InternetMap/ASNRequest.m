5888056c416c637df769c3ef2c4117ae8673bfb9
Angelina Fabbro <angelina@steamclock.com>
2013-01-15 17:40:34 -0800

    Get ASNRequest.m in sync with master branch, seems like somehow entire functions were missing? wat.

--- InternetMap/ASNRequest.m
+++ InternetMap/ASNRequest.m
@@ -23,17 +23,17 @@
 @end
 
 void callbackCurrent (
-               DNSServiceRef sdRef,
-               DNSServiceFlags flags,
-               uint32_t interfaceIndex,
-               DNSServiceErrorType errorCode,
-               const char *fullname,
-               uint16_t rrtype,
-               uint16_t rrclass,
-               uint16_t rdlen,
-               const void *rdata,
-               uint32_t ttl,
-               void *context ) {
+                      DNSServiceRef sdRef,
+                      DNSServiceFlags flags,
+                      uint32_t interfaceIndex,
+                      DNSServiceErrorType errorCode,
+                      const char *fullname,
+                      uint16_t rrtype,
+                      uint16_t rrclass,
+                      uint16_t rdlen,
+                      const void *rdata,
+                      uint32_t ttl,
+                      void *context ) {
     
     NSData* data = [NSData dataWithBytes:rdata length:strlen(rdata)+1];
     NSString* string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
@@ -105,10 +105,56 @@ void callbackCurrent (
             }];
         }
     }];
-
+    
 }
 
- 
+- (void)fetchASNForIP:(NSString*)ip index:(int)index{
+    NSArray* ipComponents = [ip componentsSeparatedByString:@"."];
+    NSString* dnsString = [NSString stringWithFormat:@"origin.asn.cymru.com"];
+    for (NSString* component in ipComponents) {
+        dnsString = [NSString stringWithFormat:@"%@.%@", component, dnsString];
+    }
+    DNSServiceRef sdRef;
+    DNSServiceErrorType res;
+    
+    NSDictionary* context = @{@"request" : self, @"index" : [NSNumber numberWithInt:index]};
+    
+    res = DNSServiceQueryRecord(
+                                &sdRef, 0, 0,
+                                [dnsString cStringUsingEncoding:NSUTF8StringEncoding],
+                                kDNSServiceType_TXT,
+                                kDNSServiceClass_IN,
+                                callbackCurrent,
+                                (__bridge void *)context
+                                );
+    
+    if (res != kDNSServiceErr_NoError) {
+        [self failedFetchingASNForIndex:index error:@"Couldn't resolve DNS."];
+    }
+    
+    DNSServiceProcessResult(sdRef);
+    /*
+     // trying to use select, so we have a timeout, just calling DNSServiceProcessREsult
+     // can block forever, but doesn't work
+     int dns_sd_fd = DNSServiceRefSockFD(sdRef);
+     fd_set readfds;
+     struct timeval tv;
+     
+     FD_ZERO(&readfds);
+     FD_SET(dns_sd_fd, &readfds);
+     tv.tv_sec = 10;
+     tv.tv_usec = 0;
+     int result = select(1, &readfds, (fd_set*)NULL, (fd_set*)NULL, &tv);
+     if ((result > 0) && FD_ISSET(dns_sd_fd, &readfds)) {
+     DNSServiceProcessResult(sdRef);
+     }
+     else {
+     [self failedFetchingASNForIndex:index error:@"Couldn't resolve DNS."];
+     }
+     */
+    
+    DNSServiceRefDeallocate(sdRef);
+}
 
 - (void)finishedFetchingASN:(int)asn forIndex:(int)index {
     NSLog(@"ASN fetched for index %i: %i", index, asn);
@@ -135,7 +181,7 @@ void callbackCurrent (
         }else {
             response(@[[NSNull null]]);
         }
-
+        
     }];
     [request startAsynchronous];
 }
@@ -203,7 +249,7 @@ void callbackCurrent (
                 [ASNRequest fetchForAddresses:@[ip] responseBlock:response];
             }
         } failedBlock:error];
-
+        
     }];
 }
 
