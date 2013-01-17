12470759603ed4db5c43995b25c3e00aa4d77f5f
Nigel Brooke <nigel@steamclocksw.com>
2013-01-17 15:59:18 -0800

    Just stick with the non-glMapBuffer path on all Android devices, it seems to be flakey.

--- InternetMap/OpenGL.cpp
+++ InternetMap/OpenGL.cpp
@@ -19,7 +19,7 @@ bool InitOpenGLExtensions(void) {
     std::string mapBufferExtension("GL_OES_mapbuffer");
     std::string extensions((char*)(glGetString(GL_EXTENSIONS)));
 
-    gHasMapBuffer = extensions.find(mapBufferExtension) != std::string::npos;
+    gHasMapBuffer = false;// extensions.find(mapBufferExtension) != std::string::npos;
     LOG("hasMapBuffer %d", (int)gHasMapBuffer);
 
     if(gHasMapBuffer) {
