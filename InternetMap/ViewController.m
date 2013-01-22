272a59b6ebe0038a6335407ddfca3d85393fe10c
Nigel Brooke <nigel@steamclocksw.com>
2013-01-22 14:00:22 -0800

    Fix bad padding value for timeline.

--- InternetMap/ViewController.m
+++ InternetMap/ViewController.m
@@ -197,7 +197,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
     int max = [[sortedYears lastObject] intValue];
     float diff = max-self.minTimelineYear;
     diff /= 10;
-    diff += 0.99; // If we don't add a buffer, current year is only the very last position on the slider
+    diff += 0.099; // If we don't add a buffer, current year is only the very last position on the slider
     self.timelineSlider.minimumValue = 0;
     self.timelineSlider.maximumValue = diff;
     self.timelineSlider.value = diff;
