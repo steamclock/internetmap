0ac5f4b0065fed6c8322463ac756fa7feeedb012
Chani Armitage <chani@steamclock.com>
2013-02-13 16:33:50 -0800

    Remove highlight code from info popup. Android half of #241

--- Android/src/com/peer1/internetmap/InfoPopup.java
+++ Android/src/com/peer1/internetmap/InfoPopup.java
@@ -1,45 +1,22 @@
 package com.peer1.internetmap;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 
-//note: a lot of this is c&p from VisualizationPopupWindow. if we get a 3rd simple menu we should abstract it out.
 public class InfoPopup extends PopupWindow{
     private static String TAG = "InfoPopup";
-    private Context mContext;
-
-    private class InfoArrayAdapter extends ArrayAdapter<String> {
-        public int selectedRow;
-
-        public InfoArrayAdapter(Context context, int resource, int textViewResourceId, String[] objects) {
-            super(context, resource, textViewResourceId, objects);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            TextView textView = (TextView) super.getView(position, convertView, parent);
-            if (position == selectedRow) {
-                textView.setBackgroundColor(mContext.getResources().getColor(R.color.orange));
-                textView.setTextColor(Color.BLACK);
-            } else {
-                textView.setBackgroundColor(Color.TRANSPARENT);
-                textView.setTextColor(Color.WHITE);
-            }
-            return textView;
-        }
-    }
 
     public InfoPopup(final InternetMap context, final MapControllerWrapper controller, View view) {
         super(view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         setBackgroundDrawable(new ColorDrawable(context.getResources().getColor(R.color.translucentBlack)));
         setOutsideTouchable(true);
         setFocusable(true);
-        mContext = context;
 
         final ListView listView = (ListView) getContentView().findViewById(R.id.visualizationList);
         
@@ -48,7 +25,7 @@ public class InfoPopup extends PopupWindow{
         values[1] = context.getString(R.string.infoSales);
         values[2] = context.getString(R.string.infoCredits);
         
-        final InfoArrayAdapter adapter = new InfoArrayAdapter(context, android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, android.R.id.text1, values);
         listView.setAdapter(adapter);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
