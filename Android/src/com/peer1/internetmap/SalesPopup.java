faf6d9693ccb3c10cae6e210810ef9ad1bb22a2f
Chani Armitage <chani@steamclock.com>
2013-02-18 15:38:02 -0800

    UI tweaks for the contact form.
    Covers most of #259:
    -Placeholder texts for name and email say 'required'.
    -Phone field is last.
    -Validation is done on submit. (Sadly the API for doing it on text change was too crashy)

--- Android/src/com/peer1/internetmap/SalesPopup.java
+++ Android/src/com/peer1/internetmap/SalesPopup.java
@@ -1,17 +1,22 @@
 package com.peer1.internetmap;
 
+import android.content.Context;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 public class SalesPopup extends PopupWindow{
     private static String TAG = "SalesPopup";
+    private Context mContext;
 
     public SalesPopup(final InternetMap context, View view) {
         super(view, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         setOutsideTouchable(true);
         setFocusable(true);
+        mContext = context;
 
         View closeButton = getContentView().findViewById(R.id.closeBtn);
         closeButton.setOnClickListener(new View.OnClickListener() {
@@ -23,10 +28,35 @@ public class SalesPopup extends PopupWindow{
         View submitButton = getContentView().findViewById(R.id.submitButton);
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View arg0) {
-                Log.d(TAG, "TODO: submit");
-                SalesPopup.this.dismiss();
+                //validate (split so that we do not short-circuit past any check).
+                boolean valid = validateName();
+                valid = validateEmail() && valid;
+                if (valid) {
+                    Log.d(TAG, "TODO: submit");
+                    SalesPopup.this.dismiss();
+                }
             }
         });
+    }
+    
+    private boolean validateName() {
+        EditText nameEdit = (EditText) getContentView().findViewById(R.id.nameEdit);
+        if (nameEdit.getText().length() == 0) {
+            //ideally we'd use setError, but it's unbelievably buggy, so we're stuck with a toast :P
+            Toast.makeText(mContext, mContext.getString(R.string.requiredName),  Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateEmail() {
+        EditText edit = (EditText) getContentView().findViewById(R.id.emailEdit);
+        if (edit.getText().length() == 0) {
+            //ideally we'd use setError, but it's unbelievably buggy, so we're stuck with a toast :P
+            Toast.makeText(mContext, mContext.getString(R.string.requiredEmail),  Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
 
