754e5c38f515d54ea6295727c27424beb40a7f2c
jozero <jot@mac.com>
2017-07-17 16:41:51 -0700

    another fabric path attempt

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -4,6 +4,8 @@ echo "Uploading IPAs and dSYMs to Crashlytics"
 
 CRASHLYTICS_API_KEY=c09d52199d3485c91714d2884df0de38db71f239
 echo "Uploading to Fabric via command line"
-$BUDDYBUILD_WORKSPACE/Fabric/upload-symbols -a $CRASHLYTICS_API_KEY -p ios $BUDDYBUILD_PRODUCT_DIR
+$BUDDYBUILD_WORKSPACE/iOS/Fabric/upload-symbols -a $CRASHLYTICS_API_KEY -p ios $BUDDYBUILD_PRODUCT_DIR
+
+echo $BUDDYBUILD_WORKSPACE
 
 ./Fabric.framework/run $Crashlytics_token
