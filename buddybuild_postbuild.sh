d15cac7ccb505e161aeebcdb5b2e262f6db82f0f
jozero <jot@mac.com>
2017-07-18 10:12:25 -0700

    access framework directly in buddy build path

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -7,5 +7,5 @@ echo $Crashlytics_token
 echo $Crashlytics_private
 echo "____"
 
-$BUDDYBUILD_WORKSPACE/Fabric.framework/uploadDSYM -a $Crashlytics_token -p ios $BUDDYBUILD_PRODUCT_DIR
+./iOS/Fabric.framework/uploadDSYM -a $Crashlytics_token -p ios $BUDDYBUILD_PRODUCT_DIR
 ./iOS/Fabric.framework/run $Crashlytics_token $Crashlytics_private
