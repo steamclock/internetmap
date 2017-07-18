840a71dfae9be6f8a9412722279e0a13f8d1beaa
jozero <jot@mac.com>
2017-07-18 10:04:11 -0700

    path of framework from buddy build log changed

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -8,4 +8,4 @@ echo $Crashlytics_private
 echo "____"
 
 $BUDDYBUILD_WORKSPACE/Fabric.framework/uploadDSYM -a $Crashlytics_token -p ios $BUDDYBUILD_PRODUCT_DIR
-./Fabric.framework/run $Crashlytics_token $Crashlytics_private
+./iOS/Fabric.framework/run $Crashlytics_token $Crashlytics_private
