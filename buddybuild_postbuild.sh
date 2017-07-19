da054d6f9259466111a9ac38ca3c4c2898995e39
jozero <jot@mac.com>
2017-07-19 11:16:40 -0700

    Drop fabric dSYM upload and try straight run

--- buddybuild_postbuild.sh
+++ buddybuild_postbuild.sh
@@ -3,5 +3,4 @@
 echo "Uploading IPAs and dSYMs to Crashlytics"
 
 export BUILT_PRODUCTS_DIR=$BUDDYBUILD_PRODUCT_DIR
-./iOS/Fabric.framework/uploadDSYM -a $Crashlytics_token -p ios BUILT_PRODUCTS_DIR
 ./iOS/Fabric.framework/run $Crashlytics_token $Crashlytics_private
