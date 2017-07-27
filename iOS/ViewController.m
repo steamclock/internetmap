1eb9175b541c07a6b47d4d734e631ea8a881825f
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2017-07-27 16:50:52 -0700

    Fixed Globe orientation issue on traceroute #443
    * Make use of resetZoomAndRotationAnimatedForOrientation instead of manually setting a rotation matrix
    * Added _suppressReset property to allow us to ignore the camera reset callback; was causing NodeInformationView to redraw on traceroute, effectively removing the traceroute details

--- iOS/ViewController.m
+++ iOS/ViewController.m
@@ -100,6 +100,8 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 @property (strong, nonatomic) NSSet* simulatedYears;
 @property (strong, nonatomic) NSArray *popMenuInfo;
 
+@property (nonatomic) BOOL suppressReset;
+
 @end
 
 @implementation ViewController
@@ -318,7 +320,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
     if (![self.controller selectHoveredNode]) { //couldn't select node
         if(self.controller.targetNode != INT_MAX) {
             [self.controller deselectCurrentNode];
-            [self.controller resetZoomAndRotationAnimatedForOrientation:![HelperMethods deviceIsiPad]];
+            [self resetVisualization];
         }
     }	
 }
@@ -614,7 +616,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
             [self helpPopCheckOrMenuSelected:nil];
             
             // Reset view to recenter target
-            [self.controller resetZoomAndRotationAnimatedForOrientation:![HelperMethods deviceIsiPad]];
+            [self resetVisualization];
         };
     }
     [self.visualizationSelectionPopover presentPopoverFromRect:self.visualizationsButton.bounds inView:self.visualizationsButton permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
@@ -634,7 +636,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 
 -(IBAction)infoButtonPressed:(id)sender {
     
-    [self.controller resetZoomAndRotationAnimatedForOrientation:YES];
+    [self resetVisualization];
     
     self.helpPopView.hidden = YES;
     
@@ -750,6 +752,12 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 }
 
 -(void)displayInformationPopoverForCurrentNode {
+    
+    if (_suppressReset) {
+        _suppressReset = false;
+        return;
+    }
+    
     NodeWrapper* node;
     
     if(self.controller.targetNode != INT_MAX) {
@@ -870,7 +878,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 -(void) resetView {
     [self dismissNodeInfoPopover];
     [self.controller deselectCurrentNode];
-    [self.controller resetZoomAndRotationAnimatedForOrientation:![HelperMethods deviceIsiPad]];
+    [self resetVisualization];
 }
 
 - (void)viewResetDone{
@@ -949,7 +957,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 #pragma mark - NodeInfo delegate
 
 - (void)dismissNodeInfoPopover {
-
+    
     [self.tracer stop];
     self.tracer = nil;
     [self.nodeInformationPopover dismissPopoverAnimated:YES];
@@ -960,6 +968,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
     }
     
     [self helpPopCheckOrMenuSelected:nil];
+
 }
 
 #pragma mark - Node Info View Delegate
@@ -988,28 +997,31 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
     
     self.tracerouteASNs = [NSMutableDictionary new];
     
+    _suppressReset = YES;
+    [self resetVisualization];
+    
     //zoom out and rotate camera to default orientation on app startup
     // TODO Why is this using GLKMatrix4MakeRotation, and not calling the methods in MapWrapper?
-    GLKMatrix4 zRotation = GLKMatrix4Identity;
-    float zoom = -3;
-    if (![HelperMethods deviceIsiPad]) {
-        zRotation = GLKMatrix4MakeRotation(M_PI_2, 0, 0, 1);
-        zoom = -6;
-    }
+//    GLKMatrix4 zRotation = GLKMatrix4Identity;
+//    float zoom = -3;
+//    if (![HelperMethods deviceIsiPad]) {
+//        zRotation = GLKMatrix4MakeRotation(M_PI_2, 0, 0, 1);
+//        zoom = -6;
+//    }
     
-    [self.controller zoomAnimated:zoom duration:3];
+    //[self.controller zoomAnimated:zoom duration:3];
     
     // On phones, translate up view so that we can more easily see it
     if (![HelperMethods deviceIsiPad]) {
-        [self.controller translateYAnimated:0.5f duration:3];
+        [self.controller translateYAnimated:0.25f duration:3];
     }
     
-    NodeWrapper* node = [self.controller nodeAtIndex:self.controller.targetNode];
-    if (node.importance > 0.006) {
-        [self.controller rotateAnimated:zRotation duration:3];
-    } else {
-        [self.controller rotateAnimated:GLKMatrix4Multiply(GLKMatrix4MakeRotation(M_PI, 0, 1, 0), zRotation) duration:3];
-    }
+//    NodeWrapper* node = [self.controller nodeAtIndex:self.controller.targetNode];
+//    if (node.importance > 0.006) {
+//        [self.controller rotateAnimated:zRotation duration:3];
+//    } else {
+//        [self.controller rotateAnimated:GLKMatrix4Multiply(GLKMatrix4MakeRotation(M_PI, 0, 1, 0), zRotation) duration:3];
+//    }
     
     if(self.controller.lastSearchIP && ![self.controller.lastSearchIP isEqualToString:@""]) {
         self.tracer = [SCTracerouteUtility tracerouteWithAddress:self.controller.lastSearchIP];
@@ -1059,7 +1071,19 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 
     [self dismissNodeInfoPopover];
     [self.controller deselectCurrentNode];
+    [self resetVisualization];
+    
+}
+
+-(void)resetVisualization {
+    
     [self.controller resetZoomAndRotationAnimatedForOrientation:![HelperMethods deviceIsiPad]];
+    
+    // On phones, translate up view so that we can more easily see it
+    if (![HelperMethods deviceIsiPad]) {
+        [self.controller translateYAnimated:0.0f duration:1];
+    }
+    
 }
 
 #pragma mark - help pop
