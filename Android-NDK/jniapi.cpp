dc1a549483515de54f4b1c8fdc06004438a3afea
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2017-09-06 17:24:12 -0700

    Basic ICMP Ping working
    * Removed hard coded netinet folder, now using platform dependent libs
    * Added basic ping to MapControllerWrapper JNI

--- Android-NDK/jniapi.cpp
+++ Android-NDK/jniapi.cpp
@@ -10,6 +10,16 @@
 #include <../Common/Code/MapDisplay.hpp>
 #include <../Common/Code/Camera.hpp>
 
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+#include <sys/select.h>
+
 static ANativeWindow *window = 0;
 static Renderer *renderer = 0;
 
@@ -287,6 +297,120 @@ JNIEXPORT jstring JNICALL Java_com_peer1_internetmap_NodeWrapper_nativeFriendlyD
     jstring ret = jenv->NewStringUTF(node->friendlyDescription().c_str());
     renderer->endControllerModification();
     return ret;
+}
+
+void printIcmpHdr(char* title, icmp icmp_hdr) {
+    LOG("%s: type=0x%x, id=0x%x, sequence =  0x%x\n",
+    title, icmp_hdr.icmp_type, icmp_hdr.icmp_id, icmp_hdr.icmp_seq);
+}
+
+/**
+ * https://stackoverflow.com/questions/8290046/icmp-sockets-linux
+ * @param dst
+ */
+void ping_it(struct in_addr *dst)
+{
+    struct icmp icmp_hdr;
+    struct sockaddr_in addr;
+
+    int sequence = 0;
+    int sock = socket(AF_INET,SOCK_DGRAM,IPPROTO_ICMP);
+    if (sock < 0) {
+        perror("socket");
+        return ;
+    }
+
+    memset(&addr, 0, sizeof addr);
+    addr.sin_family = AF_INET;
+    addr.sin_addr = *dst;
+
+    memset(&icmp_hdr, 0, sizeof icmp_hdr);
+    icmp_hdr.icmp_type = ICMP_ECHO;
+    //icmp_hdr.un.echo.id = 1234;//arbitrary id
+
+    for (;;) {
+        unsigned char data[2048];
+        int rc;
+        struct timeval timeout = {3, 0}; //wait max 3 seconds for a reply
+        fd_set read_set;
+        socklen_t slen;
+        struct icmp rcv_hdr;
+
+        icmp_hdr.icmp_seq = sequence++;
+        memcpy(data, &icmp_hdr, sizeof icmp_hdr);
+        memcpy(data + sizeof icmp_hdr, "hello", 5); //icmp payload
+        rc = sendto(sock, data, sizeof icmp_hdr + 5,
+                    0, (struct sockaddr*)&addr, sizeof addr);
+
+        printIcmpHdr("Send   ", icmp_hdr);
+
+        if (rc <= 0) {
+            LOG("Sendto");
+            break;
+        }
+
+        LOG("Sent ICMP");
+
+        memset(&read_set, 0, sizeof read_set);
+        FD_SET(sock, &read_set);
+
+        //wait for a reply with a timeout
+        rc = select(sock + 1, &read_set, NULL, NULL, &timeout);
+        if (rc == 0) {
+            LOG("Got no reply");
+            continue;
+        } else if (rc < 0) {
+            LOG("Select");
+            break;
+        }
+
+        //we don't care about the sender address in this example..
+        slen = 0;
+        rc = recvfrom(sock, data, sizeof data, 0, NULL, &slen);
+
+        LOG("Receive length, %d bytes\n", slen);
+
+        if (rc <= 0) {
+            LOG("recvfrom");
+            break;
+        } else if (rc < sizeof rcv_hdr) {
+            LOG("Error, got short ICMP packet, %d bytes\n", rc);
+            break;
+        }
+
+        memcpy(&rcv_hdr, data, sizeof rcv_hdr);
+        if (rcv_hdr.icmp_type == ICMP_ECHOREPLY) {
+            printIcmpHdr("Receive", rcv_hdr);
+        } else {
+            LOG("Got ICMP packet with type 0x%x ?!?\n", rcv_hdr.icmp_type);
+        }
+    }
+}
+
+JNIEXPORT void JNICALL Java_com_peer1_internetmap_MapControllerWrapper_sendPacket(JNIEnv* jenv, jobject obj) {
+
+    LOG("traceroute start");
+
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+
+    if (sock < 0) {
+        LOG("Failed to create socket");
+        return;
+    }
+
+    struct in_addr testaddr;
+    inet_aton("13.32.253.9", &testaddr);
+
+    ping_it(&testaddr);
+
+    //----
+    // https://books.google.ca/books?id=JYhA5uqOxIAC&pg=PA253&lpg=PA253&dq=icmp6_filt+example&source=bl&ots=PdgcvSgI_H&sig=it25MHd-k8ZcLg2ZEJO8WTy0RJU&hl=en&sa=X&ved=0ahUKEwis9qjyupHWAhUB9GMKHdBuDnwQ6AEIOzAE#v=onepage&q=icmp6_filt%20example&f=false
+    //struct icmp6_filter filter;
+    //ICMP6_FILTER_SETBLOCKALL(&filter);
+    //ICMP6_FILTER_SETPASS(ND_ROUTER_ADVERT, &filter);
+    //setsockopt(sock, IPPROTO_ICMPV6, ICMP_FILTER, &filter, sizeof(&filter));
+    //----
+
 }
 
 void DetachThreadFromVM(void) {
