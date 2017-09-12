14233fda65f64479e38a5b27c99312eca0c24d87
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2017-09-12 14:08:22 -0700

    getASNFromIP now using herokuapp

--- Android/app/src/main/java/com/peer1/internetmap/network/common/CommonAPI.java
+++ Android/app/src/main/java/com/peer1/internetmap/network/common/CommonAPI.java
@@ -37,22 +37,14 @@ public interface CommonAPI {
      * @param ip The IP address
      * @return Call will return an ASN object which contains information about the AS.
      */
-    @GET("https://api.iptoasn.com/v1/as/ip/{ip}")
-    Call<ASN> getASNFromIP(@Path("ip") String ip);
+    @GET("https://internetmap-server.herokuapp.com/?req=iptoasn")
+    Call<ASN> getASNFromIP(@Query("ip") String ip);
 
     /**
      * Get a list of IPs controlled by a given Autonomous System (AS) number.
      * <p>
-     * https://iptoasn.com/
-     * Free IP address to ASN database
-     * Can also download DB and run local service
-     * <p>
-     * TODO Need replacement for getting a list of IPs from an ASN. mxtoolbox does not suit our needs.
-     * <p>
-     * @param asn The Autonomous System Number (ASN)
-     * @param auth Auth token for the service
-     * @return Call will return an MxASNInfo object which contains blocks of IPs controlled by the ASN.
+     * TODO switch to use https://internetmap-server.herokuapp.com/?req=asntoips&asn=4565
      */
-    @GET("https://api.mxtoolbox.com/api/v1/lookup/asn/as{asn}")
-    Call<MxASNInfo> getIPFromASN(@Path("asn") String asn, @Query("authorization") String auth);
+    //@GET("https://api.mxtoolbox.com/api/v1/lookup/asn/as{asn}")
+    //Call<MxASNInfo> getIPFromASN(@Path("asn") String asn, @Query("authorization") String auth);
 }
