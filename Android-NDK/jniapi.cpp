a7e52db2e868b233095d5ef0064cb2ea19531b45
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2017-09-14 17:15:07 -0700

    temp

--- Android-NDK/jniapi.cpp
+++ Android-NDK/jniapi.cpp
@@ -15,7 +15,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
+#include <linux/types.h>
+#include <linux/errqueue.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
 #include <arpa/inet.h>
 #include <sys/select.h>
@@ -25,6 +28,8 @@ static Renderer *renderer = 0;
 
 static jobject activity = 0;
 static JavaVM* javaVM;
+
+#define HOST_COLUMN_SIZE	52
 
 jint JNI_OnLoad(JavaVM* vm, void* reserved)
 {
@@ -419,6 +424,320 @@ void ping_it(struct in_addr *dst)
     }
 }
 
+
+
+
+
+
+
+int setupSocket(struct in_addr *dst) {
+    int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+    if (sock < 0) {
+        perror("socket");
+        return sock;
+    }
+
+    int on = IP_PMTUDISC_PROBE;
+    if (setsockopt(sock, SOL_IP, IP_MTU_DISCOVER, &on, sizeof(on)) && (on = IP_PMTUDISC_DO, setsockopt(sock, SOL_IP, IP_MTU_DISCOVER, &on, sizeof(on)))) {
+        LOG("IP_MTU_DISCOVER");
+        //exit(1);
+    }
+    on = 1;
+
+    /**
+     * SOL_IP (set/configure various IP packet options, IP layer behaviors, [as here] netfilter module options)
+     */
+
+    if (setsockopt(sock, SOL_IP, IP_RECVERR, &on, sizeof(on))) {
+        LOG("IP_RECVERR");
+        //exit(1);
+    }
+    if (setsockopt(sock, SOL_IP, IP_RECVTTL, &on, sizeof(on))) {
+        LOG("IP_RECVTTL");
+        //exit(1);
+    }
+
+    return sock;
+}
+
+int waitForReply(int sock) {
+    // Wait for a reply with a timeout
+    fd_set fds;
+    struct timeval tv;
+    FD_ZERO(&fds);
+    FD_SET(sock, &fds);
+    tv.tv_sec = 3;
+    tv.tv_usec = 0;
+    return select(sock+1, &fds, NULL, NULL, &tv);
+}
+
+
+struct probehdr
+{
+    __u32 ttl;
+    struct timeval tv;
+};
+
+int receiveError(int sock, int ttl) {
+    struct msghdr msg;
+    struct probehdr rcvbuf;
+    struct iovec  iov;
+    struct sockaddr_in addr;
+    char cbuf[512];
+
+    memset(&rcvbuf, -1, sizeof(rcvbuf));
+    iov.iov_base = &rcvbuf;
+    iov.iov_len = sizeof(rcvbuf);
+    msg.msg_name = (__u8*)&addr;
+    msg.msg_namelen = sizeof(addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    msg.msg_flags = 0;
+    msg.msg_control = cbuf;
+    msg.msg_controllen = sizeof(cbuf);
+
+    int res = recvmsg(sock, &msg, MSG_ERRQUEUE);
+    if (res < 0) {
+        if (errno == EAGAIN) {
+            LOG("errno == EAGAIN");
+            return res; //return progress;
+        }
+    }
+
+//    if (res == sizeof(rcvbuf)) {
+//        if (rcvbuf.ttl == 0 || rcvbuf.tv.tv_sec == 0) {
+//            broken_router = 1;
+//        } else {
+//            sndhops = rcvbuf.ttl;
+//            rettv = &rcvbuf.tv;
+//        }
+//    }
+
+    struct cmsghdr *cmsg;
+    struct sock_extended_err *e;
+    int rethops;
+    int sndhops;
+
+    e = NULL;
+
+    for (cmsg = CMSG_FIRSTHDR(&msg); cmsg; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+        if (cmsg->cmsg_level == SOL_IP) {
+            if (cmsg->cmsg_type == IP_RECVERR) {
+                e = (struct sock_extended_err *) CMSG_DATA(cmsg);
+            } else if (cmsg->cmsg_type == IP_TTL) {
+                memcpy(&rethops, CMSG_DATA(cmsg), sizeof(rethops));
+            } else {
+                printf("cmsg:%d\n ", cmsg->cmsg_type);
+            }
+        }
+    }
+
+    if (e == NULL) {
+        printf("no info\n");
+        return 0;
+    }
+
+    if (e->ee_origin == SO_EE_ORIGIN_LOCAL) {
+        LOG("%2d?: %*s ", ttl, -(HOST_COLUMN_SIZE - 1), "[LOCALHOST]");
+    } else if (e->ee_origin == SO_EE_ORIGIN_ICMP) {
+        char abuf[128];
+        struct sockaddr_in *sin = (struct sockaddr_in*)(e+1);
+        //struct hostent *h = NULL;
+        //char *idn = NULL;
+        inet_ntop(AF_INET, &sin->sin_addr, abuf, sizeof(abuf));
+
+        char* rcv_addy = inet_ntoa(sin->sin_addr);
+        LOG("Receive from %s", rcv_addy);
+
+        if (sndhops>0)
+            LOG("%2d:  ", sndhops);
+        else
+            LOG("%2d?: ", ttl);
+    }
+
+    rethops = -1;
+    sndhops = -1;
+
+    switch (e->ee_errno) {
+        case ETIMEDOUT:
+            LOG("ETIMEDOUT");
+            break;
+        case EMSGSIZE:
+            LOG("EMSGSIZE");
+            LOG("pmtu %d\n", e->ee_info);
+            //mtu = e->ee_info;
+            //progress = mtu;
+            break;
+        case ECONNREFUSED:
+            LOG("ECONNREFUSED");
+            LOG("reached\n");
+            //hops_to = sndhops<0 ? ttl : sndhops;
+            //hops_from = rethops;
+            return 0;
+        case EPROTO:
+            LOG("EPROTO");
+            LOG("!P\n");
+            return 0;
+        case EHOSTUNREACH:
+            LOG("EHOSTUNREACH");
+            if (e->ee_origin == SO_EE_ORIGIN_ICMP &&
+                e->ee_type == 11 &&
+                e->ee_code == 0) {
+                if (rethops>=0) {
+                    if (rethops<=64)
+                        rethops = 65-rethops;
+                    else if (rethops<=128)
+                        rethops = 129-rethops;
+                    else
+                        rethops = 256-rethops;
+                    if (sndhops>=0 && rethops != sndhops)
+                        LOG("asymm %2d ", rethops);
+                    else if (sndhops<0 && rethops != ttl)
+                        LOG("asymm %2d ", rethops);
+                }
+                break;
+            }
+            return 0;
+        case ENETUNREACH:
+            LOG("ENETUNREACH");
+            return 0;
+        case EACCES:
+            LOG("EACCES");
+            return 0;
+        default:
+            errno = e->ee_errno;
+            LOG("NET ERROR");
+            return 0;
+    }
+
+    // goto restart
+    return 0;
+}
+
+bool receiveData(int sock) {
+    struct sockaddr_in rcv_addr;
+    socklen_t slen = sizeof rcv_addr;
+
+    unsigned char data[2048];
+    struct icmp rcv_hdr;
+
+    int rc = recvfrom(sock, data, sizeof data, 0, (struct sockaddr*)&rcv_addr, &slen);
+    char* rcv_addy = inet_ntoa(rcv_addr.sin_addr);
+    LOG("Receive length, %d bytes from %s", slen, rcv_addy);
+
+    if (rc <= 0) {
+        LOG("Failed to recvfrom");
+        return false;
+    } else if (rc < sizeof rcv_hdr) {
+        LOG("Error, got short ICMP packet, %d bytes", rc);
+        return false;
+    }
+
+    memcpy(&rcv_hdr, data, sizeof rcv_hdr);
+    if (rcv_hdr.icmp_type == ICMP_ECHOREPLY) {
+        printIcmpHdr("Received", rcv_hdr);
+        return true;
+    } else {
+        LOG("Got ICMP packet with type 0x%x ?!?", rcv_hdr.icmp_type);
+    }
+
+    return false;
+}
+
+bool sendProbe(int sock, sockaddr_in addr, int ttl) {
+
+    struct icmp icmp_hdr;
+    int sequence = 0;
+
+    LOG("-------------------------------------------");
+    memset(&icmp_hdr, 0, sizeof icmp_hdr);
+    icmp_hdr.icmp_type = ICMP_ECHO;
+    //icmp_hdr.un.echo.id = 1234;//arbitrary id
+
+    //for (int i=0; i < 10; i++) { // I don't understand the loop to 10 here.
+
+        //icmp_hdr.icmp_seq = sequence++;
+        unsigned char data[2048];
+        memcpy(data, &icmp_hdr, sizeof icmp_hdr);
+        memcpy(data + sizeof icmp_hdr, "hello", 5); //icmp payload
+
+        printIcmpHdr("Sending   ", icmp_hdr);
+        int rc = sendto(sock, data, sizeof icmp_hdr + 5, 0, (struct sockaddr*)&addr, sizeof addr);
+        if (rc <= 0) {
+            LOG("Failed to send ICMP packet");
+            return false;
+        }
+
+        int err = receiveError(sock, ttl);
+        if (err == 0) {
+            return false;
+        }
+
+        LOG("Sent ICMP");
+
+    //}
+
+    //if (i < 10) {
+
+    int reply = waitForReply(sock);
+    if (reply == 0) {
+        LOG("waitForReply, no reply");
+        return false;
+    } else if (reply < 0) {
+        LOG("Failed waitForReply");
+        return false;
+    } else {
+        return receiveData(sock);
+    }
+
+    //}
+}
+
+void tracepath(struct in_addr *dst)
+{
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof addr);
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr = *dst;
+    char* snd_addy = inet_ntoa(addr.sin_addr);
+
+    LOG("----------------------------------------------");
+    LOG(" Starting trace to %s", snd_addy);
+
+    int sock = setupSocket(dst);
+    if (sock < 0) {
+        LOG("Failed to build send socket");
+        return;
+    }
+
+    int maxHops = 255;
+
+    for (int ttl = 1; ttl < maxHops; ttl++) {
+
+        // Set TTL on socket
+        if (setsockopt(sock, SOL_IP, IP_TTL, &ttl, sizeof(ttl))) {
+            LOG("Failed to set IP_TTL");
+            return;
+        }
+
+        // Send up to 3 probes
+        for (int probeCount = 0; probeCount < 1; probeCount++) {
+
+            if (sendProbe(sock, addr, ttl)) {
+                ttl = 10000; //break; // break;
+            }
+
+        }
+    }
+
+    LOG("----------------------------------------------");
+    LOG(" Trace complete");
+    LOG("----------------------------------------------");
+}
+
+
+
 JNIEXPORT void JNICALL Java_com_peer1_internetmap_MapControllerWrapper_sendPacket(JNIEnv* jenv, jobject obj) {
 
     LOG("traceroute start");
@@ -436,7 +755,8 @@ JNIEXPORT void JNICALL Java_com_peer1_internetmap_MapControllerWrapper_sendPacke
     struct in_addr testaddr;
     inet_aton("172.217.3.164"/*"13.32.253.9"*/, &testaddr);
 
-    ping_it(&testaddr);
+    //ping_it(&testaddr);
+    tracepath(&testaddr);
 
     //----
     // https://books.google.ca/books?id=JYhA5uqOxIAC&pg=PA253&lpg=PA253&dq=icmp6_filt+example&source=bl&ots=PdgcvSgI_H&sig=it25MHd-k8ZcLg2ZEJO8WTy0RJU&hl=en&sa=X&ved=0ahUKEwis9qjyupHWAhUB9GMKHdBuDnwQ6AEIOzAE#v=onepage&q=icmp6_filt%20example&f=false
