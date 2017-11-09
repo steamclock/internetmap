82559199f439776331506d62746af43a96817650
Shayla Sawchenko <shayla.sawchenko@recursecreative.com>
2017-11-09 15:32:42 -0800

    Updated CreditsViewController to conform to safe areas

--- iOS/CreditsViewController.swift
+++ iOS/CreditsViewController.swift
@@ -12,7 +12,7 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
     public var informationType = ""
     public var delegate: AnyObject?
 
-    private var webView: UIWebView?
+    private var webView: UIWebView!
     private var aboutMoreButton: UIButton?
     
     class func currentSize() -> CGSize {
@@ -27,15 +27,11 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
             size = CGSize(width: size.height, height: size.width)
         }
 
-        if application.isStatusBarHidden == false {
-            size.height -= min(application.statusBarFrame.size.width, application.statusBarFrame.size.height)
-        }
-
         return size
     }
 
     override public func viewDidLoad() {
-        // Background image
+// Background image
         var backgroundImage: UIImage?
 
         if UIDevice.current.userInterfaceIdiom == .phone {
@@ -64,13 +60,13 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
             // webViewFrame.origin.x += 300;
             webViewFrame.origin.x = UIScreen.main.bounds.size.width / 2 - 200
             webViewFrame.size.width -= 600
-            webView?.scrollView.isScrollEnabled = false
+            webView.scrollView.isScrollEnabled = false
         }
         else {
             webViewFrame.size.width = UIScreen.main.bounds.size.width - 20
         }
 
-        webView?.frame = webViewFrame
+        webView.frame = webViewFrame
 
         var filePath: String
 
@@ -87,34 +83,56 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
         let html = try? String(contentsOfFile: filePath, encoding: .utf8)
 
         if html != nil {
-            webView?.loadHTMLString(html ?? "", baseURL: nil)
+            webView.loadHTMLString(html ?? "", baseURL: nil)
         }
 
-        webView?.backgroundColor = UIColor.clear
-        webView?.isOpaque = false
-        webView?.scrollView.showsVerticalScrollIndicator = false
-        webView?.scrollView.indicatorStyle = .white
+        webView.backgroundColor = UIColor.clear
+        webView.isOpaque = false
+        webView.scrollView.showsVerticalScrollIndicator = false
+        webView.scrollView.indicatorStyle = .white
         // Start webview faded out, load happens async, and this way we can fade it in rather
         // than popping when the load finishes. Slightly less jarring that way.
-        webView?.alpha = 0.00
-        webView?.delegate = self
-        view.addSubview(webView ?? UIView())
+        webView.alpha = 0.00
+        webView.delegate = self
+        view.addSubview(webView)
 
         //Done button
         let xImage = UIImage(named: "x-icon")!
+        let doneButtonWidth = xImage.size.width + 20
+        let doneButtonHeight = xImage.size.height + 20
         let doneButton = UIButton(type: .custom)
-        let doneFrame = CGRect(x: UIScreen.main.bounds.size.width - (xImage.size.width + 20), y: 0, width: xImage.size.width + 20, height: xImage.size.height + 20)
+        let doneFrame = CGRect(x: UIScreen.main.bounds.size.width - (xImage.size.width + 20), y: 20, width: doneButtonWidth, height: doneButtonHeight)
         doneButton.frame = doneFrame
         doneButton.imageView?.contentMode = .center
         doneButton.setImage(xImage, for: .normal)
         doneButton.addTarget(self, action: #selector(self.close), for: .touchUpInside)
         doneButton.backgroundColor = Theme.primary
+        doneButton.layer.cornerRadius = doneButton.frame.size.height / 2
         view.addSubview(doneButton)
+
+        // iPhoneX support
+        if #available(iOS 11.0, *) {
+            let guide = self.view.safeAreaLayoutGuide
+
+            doneButton.translatesAutoresizingMaskIntoConstraints = false
+            doneButton.topAnchor.constraint(equalTo: guide.topAnchor, constant:10).isActive = true
+            doneButton.heightAnchor.constraint(equalToConstant: doneButtonHeight).isActive = true
+            doneButton.widthAnchor.constraint(equalToConstant: doneButtonWidth).isActive = true
+            doneButton.rightAnchor.constraint(equalTo: guide.rightAnchor, constant: -15).isActive = true
+
+            webView.translatesAutoresizingMaskIntoConstraints = false
+            webView.topAnchor.constraint(equalTo: guide.topAnchor, constant:0).isActive = true
+            webView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant:0).isActive = true
+            webView.leftAnchor.constraint(equalTo: guide.leftAnchor, constant:20).isActive = true
+            webView.rightAnchor.constraint(equalTo: guide.rightAnchor, constant:-20).isActive = true
+        }
+
         super.viewDidLoad()
     }
     
     func createContactButtonForAbout() {
         let aboutMoreButton = UIButton(type: .system)
+        let aboutMoreButtonHeight: CGFloat = 50
         aboutMoreButton.setTitleColor(UIColor.white, for: .normal)
         aboutMoreButton.setTitle(NSLocalizedString("Visit cogecopeer1.com", comment: ""), for: .normal)
         aboutMoreButton.backgroundColor = Theme.primary
@@ -123,7 +141,7 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
         if UIScreen.main.bounds.size.width > 300 {
             contactButtonWidth = 300
         }
-        let contactFrame = CGRect(x: UIScreen.main.bounds.size.width / 2 - (contactButtonWidth / 2), y: UIScreen.main.bounds.size.height - 60, width: contactButtonWidth, height: 45)
+        let contactFrame = CGRect(x: UIScreen.main.bounds.size.width / 2 - (contactButtonWidth / 2), y: UIScreen.main.bounds.size.height - 60, width: contactButtonWidth, height: aboutMoreButtonHeight)
         aboutMoreButton.frame = contactFrame
         aboutMoreButton.layer.cornerRadius = aboutMoreButton.frame.size.height / 2
         aboutMoreButton.addTarget(self, action: #selector(self.aboutMore), for: .touchUpInside)
@@ -131,6 +149,16 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
         self.aboutMoreButton = aboutMoreButton
 
         view.addSubview(aboutMoreButton)
+
+        // iPhoneX support
+        if #available(iOS 11.0, *) {
+            aboutMoreButton.translatesAutoresizingMaskIntoConstraints = false
+            let guide = self.view.safeAreaLayoutGuide
+            aboutMoreButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant:-10).isActive = true
+            aboutMoreButton.heightAnchor.constraint(equalToConstant: aboutMoreButtonHeight).isActive = true
+            aboutMoreButton.widthAnchor.constraint(equalToConstant: contactButtonWidth).isActive = true
+            aboutMoreButton.centerXAnchor.constraintEqualToSystemSpacingAfter(guide.centerXAnchor, multiplier: 1.0).isActive = true
+        }
     }
 
     public func webViewDidFinishLoad(_ webView: UIWebView) {
@@ -165,14 +193,10 @@ public class CreditsViewController: UIViewController, UIWebViewDelegate {
 
     override public func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        // the suggested not depreciated call does not seem to work
-        UIApplication.shared.setStatusBarHidden(true, with: .none)
     }
 
     override public func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        // the suggested not depreciated call does not seem  to work
-        UIApplication.shared.setStatusBarHidden(false, with: .none)
     }
 
 }
