0cb8cf84b0b320ed7d89a3fb34f63a94daa7f057
Rob MacEachern <rob@robmaceachern.com>
2018-03-06 14:00:32 -0800

    Mark tooltips as shown if the corresponding menu button was activated

--- iOS/ViewController.m
+++ iOS/ViewController.m
@@ -1262,7 +1262,7 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
 
 -(void) helpPopCheckOrMenuSelected:(UIButton*)menuButton {
     
-    NSMutableString *shownMenusPopsString = [[NSUserDefaults standardUserDefaults] objectForKey:@"helpPopMenusShownDefault"];
+    NSString *shownMenusPopsString = [[NSUserDefaults standardUserDefaults] objectForKey:@"helpPopMenusShownDefault"];
     NSRange range = [shownMenusPopsString rangeOfString:@"0"];
     if (range.location == NSNotFound) return; // all help pops have been shown
     
@@ -1309,10 +1309,9 @@ BOOL UIGestureRecognizerStateIsActive(UIGestureRecognizerState state) {
             }
             currentLocation++;
         }
-        if (currentLocation != helpLocation) return;
-        
+
         // the menu with the tooltip has been viewed, mark as 1
-        shownMenusPopsString = (NSMutableString *)[shownMenusPopsString stringByReplacingCharactersInRange:NSMakeRange(helpLocation, 1) withString:@"1"];
+        shownMenusPopsString = [shownMenusPopsString stringByReplacingCharactersInRange:NSMakeRange(currentLocation, 1) withString:@"1"];
         [[NSUserDefaults standardUserDefaults] setObject:shownMenusPopsString forKey:@"helpPopMenusShownDefault"];
         [[NSUserDefaults standardUserDefaults] synchronize];
     }
