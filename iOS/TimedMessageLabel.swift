197ecf19b84ad55e1f168a743724ee76997e6ca3
Nigel Brooke <nigel.brooke@gmail.com>
2018-03-06 14:33:27 -0800

    Stop error message from blocking the buttons on the iPhone X, and other cleanup of that view. (#560)
    - Add it to storyboard / auto layout, so it can respond to the tall top of the screen
    - Refactor the structure of the code / views to be simpler
    - Convert that view to Swift
    - Improve the aesthetics of the error message slightly (centre it, use the standard font)

---
+++ iOS/TimedMessageLabel.swift
@@ -0,0 +1,35 @@
+//
+//  TimedMessageLabel.swift
+//  Internet Map
+//
+//  Created by Nigel Brooke on 2018-03-06.
+//  Copyright © 2018 Peer1. All rights reserved.
+//
+
+import UIKit
+
+public class TimedMessageLabel: UILabel {
+    private var hidingTimer: Timer?
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        alpha = 0.0
+    }
+
+    public func setErrorString(_ error: String) {
+        text = error
+
+        hidingTimer?.invalidate()
+        hidingTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.hidingTimerFired), userInfo: nil, repeats: false)
+
+        UIView.animate(withDuration: 0.75) {
+            self.alpha = 1.0
+        }
+    }
+
+    @objc private func hidingTimerFired() {
+        UIView.animate(withDuration: 0.75) {
+            self.alpha = 0.0
+        }
+    }
+}
